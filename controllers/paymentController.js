40fdb0239e748539deb3448b5e1e8f6beaa7f222
divyansh14-yadav <110622153+divyansh14-yadav@users.noreply.github.com>
2025-08-06 13:42:14 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -45,10 +45,10 @@ export const createCheckoutSession = async (req, res) => {
       success_url: `http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `http://localhost:5173/cancel`,
       customer_email: user.email,
-      metadata: {
-        userId: userId,
-        planType: planType
-      },
+      // metadata: {
+      //   userId: userId,
+      //   planType: planType
+      // },
       subscription: {
         metadata: {
           userId: userId,
