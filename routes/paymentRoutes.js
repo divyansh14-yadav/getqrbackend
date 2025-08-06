25a0159c20d5dcb1a91cce043810cb66a02ffc5f
divyansh14-yadav <110622153+divyansh14-yadav@users.noreply.github.com>
2025-08-06 15:54:06 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,10 +1,21 @@
+// import express from 'express';
+// import { createCheckoutSession, createPortalSession, handleWebhook } from '../controllers/paymentController.js';
+// import authMiddleware from "../middleware/authMiddleware.js";
+// const router = express.Router();
+
+// router.post('/create-checkout-session', authMiddleware, createCheckoutSession);
+// router.post('/create-portal-session', authMiddleware, createPortalSession);
+// router.post('/webhook', express.raw({ type: 'application/json' }), handleWebhook);
+
+// export default router;
+
 import express from 'express';
 import { createCheckoutSession, createPortalSession, handleWebhook } from '../controllers/paymentController.js';
 import authMiddleware from "../middleware/authMiddleware.js";
+import bodyParser from 'body-parser';
 const router = express.Router();
 
 router.post('/create-checkout-session', authMiddleware, createCheckoutSession);
 router.post('/create-portal-session', authMiddleware, createPortalSession);
-router.post('/webhook', express.raw({ type: 'application/json' }), handleWebhook);
 
 export default router;
