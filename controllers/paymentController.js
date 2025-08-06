0306d00fe14ee442910544c3c2ffce0f9f698d42
divyansh14-yadav <110622153+divyansh14-yadav@users.noreply.github.com>
2025-08-06 17:57:33 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -367,8 +367,8 @@ const handlePaymentIntentSucceeded = async (paymentIntent) => {
   // Customer se userId nikalo
   const customer = await stripe.customers.retrieve(customerId);
   const userId = customer.metadata?.userId;
-      console.log(customer,"customer)
-      console.log(userId,"userId)
+      console.log(customer,"customer")
+      console.log(userId,"userId")
                   
   
   if (userId) {
